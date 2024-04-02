0e8fc6d667bb1ed9a43e87a0710f98eb4dcd6bf3
snake9byte <snake9byte@tutamail.com>
2024-04-02 20:29:52 +0300

    Update schema

---
+++ src/ZUI/components/Input/index.js
@@ -0,0 +1,10 @@
+import { Input } from "./Input";
+import { Password } from "./Password";
+// import { Select } from "./Select";
+// import { Option } from "./Option";
+
+Input.Password = Password;
+// Input.Select = Select;
+// Input.Option = Option;
+
+export default Input;
