ff5fcca515d201e9318d4c4df4ac6cda1bfc71d5
snake9byte <snake9byte@tutamail.com>
2024-04-04 20:29:52 +0300

    fix broken links

---
+++ src/ZUI/components/Grid/Item.js
@@ -0,0 +1,26 @@
+import { GridItem } from "./Grid.styles";
+import { useThemeContext } from "../Theme";
+
+export const Item = (props) => {
+  const xs = props.xs;
+  const sm = props.sm ? props.sm : xs;
+  const md = props.md ? props.md : sm;
+  const lg = props.lg ? props.lg : md;
+  const xl = props.xl ? props.xl : lg;
+  const { theme } = useThemeContext();
+
+  return (
+    <GridItem
+      className="grid-item"
+      xs={xs}
+      sm={sm}
+      md={md}
+      lg={lg}
+      xl={xl}
+      theme={theme}
+      {...props}
+    >
+      {props.children}
+    </GridItem>
+  );
+};
