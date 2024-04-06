d375887de554289ec8d47ec62340260f9b0af7e6
snake9byte <snake9byte@tutamail.com>
2024-04-06 20:29:52 +0300

    update dependencies

---
+++ src/ZUI/components/Grid/Container.js
@@ -0,0 +1,17 @@
+import { GridContainer } from "./Grid.styles";
+
+export const Grid = props => {
+  return (
+  <GridContainer {...props}>
+    {props.children}
+  </GridContainer>
+)}
+
+Grid.defaultProps = {
+  gap: 4,
+  wrap: "wrap",
+  justify: "flex-start",
+  direction: "row",
+  align_items: "center",
+  align_content: "center",
+}
