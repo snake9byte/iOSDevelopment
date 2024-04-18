a8ee99c51f9b8b1f836ea43d66aed6cd10f7fd55
snake9byte <snake9byte@tutamail.com>
2024-04-18 20:29:52 +0300

    Improve accessibility of forms

---
+++ src/ZUI/index.js
@@ -0,0 +1,23 @@
+export { Anchor } from "./components/Anchor";
+export { Avatar } from "./components/Avatar";
+export { Button } from "./components/Button";
+export { default as Card } from "./components/Card";
+export { default as Collapse } from "./components/Collapse";
+export { default as Container } from "./components/Container";
+export { default as Grid } from "./components/Grid";
+export { Icon } from "./components/Icons";
+export { default as Input } from "./components/Input";
+export { Loading } from "./components/Loading";
+export { default as Tabs } from "./components/Tabs";
+export { Styles } from "./components/Style";
+export { Text } from "./components/Text";
+export {
+  darkTheme,
+  lightTheme,
+  CreateTheme,
+  Theme,
+  ThemeProvider,
+  useThemeContext,
+  colorsSchema
+} from "./components/Theme";
+export { hexToRgba } from "./utils";
