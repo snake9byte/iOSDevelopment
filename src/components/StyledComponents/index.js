eedd6727f3d6082fbd548e11fb1b57780c535e28
snake9byte <snake9byte@tutamail.com>
2024-05-08 20:29:52 +0300

    Add support for new authentication methods

---
+++ src/components/StyledComponents/index.js
@@ -0,0 +1,5 @@
+export { H1, H2 } from "./Titles";
+export { Code } from "./Code";
+export { Table, Row } from "./Table";
+export { ExampleCode } from "./ExampleCode";
+export { Types } from "./Types";
