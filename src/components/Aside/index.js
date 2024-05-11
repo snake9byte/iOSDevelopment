c3f7333d766e8b66c2bdfcd0a04c8c7558d93af6
snake9byte <snake9byte@tutamail.com>
2024-05-11 20:29:52 +0300

    add new styles

---
+++ src/components/Aside/index.js
@@ -0,0 +1,30 @@
+import { menu } from "../../constant/menu";
+import { Text, useThemeContext, Anchor } from "../../ZUI";
+import { Link } from "react-router-dom";
+import { AsideUI } from "./styles";
+
+export const Aside = ({ menuStatus }) => {
+  const { theme } = useThemeContext();
+  return (
+    <AsideUI menuStatus={menuStatus} theme={theme}>
+      <ul>
+        {menu.map((item) => (
+          <li key={`id${item.name}`}>
+            <h1>{item.name}</h1>
+            <ul>
+              {item.childs.sort().map((child, index) => (
+                <li key={index}>
+                  <Text text_color={theme.colors.text} decoration="none">
+                    <Anchor as={Link} to={child.path}>
+                      {child.name}
+                    </Anchor>
+                  </Text>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </AsideUI>
+  );
+};
